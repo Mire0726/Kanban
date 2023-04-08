7c1f677cf7ad4785eb3339a6905bd5cd5c9dd8e1
sumire <violeta.jin.726@gmail.com>
2023-04-08 13:48:38 +0900

    puppeteer書いてみる

--- nextjs-app/src/data/familymarts/xxx.ts
+++ nextjs-app/src/data/familymarts/xxx.ts
@@ -3,9 +3,18 @@ import { FamilymartData } from "@/types/familymart";
 export const familymartData: FamilymartData[] = [];
 
 const puppeteer = require('puppeteer')
-const path = require('path')
+// const path = require('path')
 
-const testItem = async page => {
-  console.log('############ 要素を1つ取得 ############')
+// const testItem = async page => {
+//   console.log(')
   
-}
+// }
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('nextjs-app/src/libs/html/sample/sample.html');
+
+  await browser.close();
+})();
