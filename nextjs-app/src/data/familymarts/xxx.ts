bc076e618fa04f49b14e59545b6fe640df247f79
sumire <violeta.jin.726@gmail.com>
2023-04-09 15:19:13 +0900

    sample.htmlからデータを取りたい

--- nextjs-app/src/data/familymarts/xxx.ts
+++ nextjs-app/src/data/familymarts/xxx.ts
@@ -2,19 +2,54 @@ import { FamilymartData } from "@/types/familymart";
 
 export const familymartData: FamilymartData[] = [];
 
-const puppeteer = require('puppeteer')
-// const path = require('path')
+import puppeteer, { ElementHandle } from "puppeteer";
+import * as fs from "fs";
 
-// const testItem = async page => {
-//   console.log(')
-  
-// }
+/**
+ * @param req HTTP request
+ * @param res HTTP responce
+ */
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    const url =
+      "nextjs-app/src/libs/html/sample/sample.html";
 
-  await page.goto('nextjs-app/src/libs/html/sample/sample.html');
+    if (!url) {
+      throw new Error("no url");
+    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const content = await page.content();
+    const h1 = await page.$("h1");
+    const td = await page.$("td");
 
-  await browser.close();
-})();
+    fs.writeFileSync("./src/libs/html/sample/sample2.html", content);
+
+    await browser.close();
+    res.status(200).send("OK");
+  } catch (e: any) {
+    res.status(400).send(`error:${e.message}`);
+  }
+};
+
+export default handler;
+
+const getProperty = async (element: ElementHandle<HTMLHeadingElement> | null) => {
+  if (!element) {
+    return "";
+  }
+  const content = await element.getProperty("textContent");
+  const value = await content.jsonValue();
+  return value;
+};
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   await page.goto('nextjs-app/src/libs/html/sample/sample.html');
+
+//   await browser.close();
+// })();
